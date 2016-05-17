e57eb22236a0bb5c5d0d909ea40d0f8b4b872dbb
Huyanh Hoang <huyanhh@uci.edu>
2016-05-16 21:52:37 -0700

    Added userSchema

--- server/models/db.js
+++ server/models/db.js
@@ -2,6 +2,7 @@
  * Created by huyanh on 2016. 5. 16..
  */
 var mongoose = require('mongoose');
+require('./users')
 
 var dbURI = "mongodb://128.195.54.50/GroupBDB"
 mongoose.connect(dbURI);
---
+++ server/models/users.js
@@ -0,0 +1,25 @@
+/**
+ * Created by huyanh on 2016. 5. 16..
+ */
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    id: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    uid: {type: String, unique: true, minlength: 8, maxlength: 8},
+    first_name: String,
+    middle_name: String,
+    last_name: String,
+    dob: String,
+    gender: String,
+    office: String,
+    email: String,
+    contact: String,
+    position: String,
+    title: String,
+    year_entered: String,
+    reports_to: [String],
+    patients: [String],
+    picture: String,
+    role: [String],
+});
